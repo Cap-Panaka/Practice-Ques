0cbb266d6c89d3e5cb19e15bc9885d156a38a761
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-07 20:55:42 +0500

    Create prob 2(c).c

---
+++ Spring 2024/prob 2(c).c
@@ -0,0 +1,25 @@
+/*Write a C program to extract all digits from a given string taken from user input and show the sum of those digits.
+ 
+*/
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[101];
+    int sum=0,i;
+    printf("Enter a sting: ");
+    scanf("%s",str);
+    
+    for(i=0;i<strlen(str); i++)
+    {
+        if(str[i]>='0' && str[i] <='9')
+        {
+            sum+=str[i]-'0';
+        }
+    }
+    printf("Sum= %d",sum);
+}
+
+
+
